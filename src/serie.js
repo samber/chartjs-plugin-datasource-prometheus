8d79771f310519858c4e0751789396c2b4dada99
Samuel Berthe <dev@samuel-berthe.fr>
2021-01-19 17:22:06 +0100

    :lipstick:

--- src/serie.js
+++ src/serie.js
@@ -1,23 +1,26 @@
 
 const selectLabel = (_options, serie, i) => {
+    const defaultValue = serie.metric.toString();
     if (_options.findInLabelMap) {
-        return _options.findInLabelMap(serie.metric) || serie.metric.toString();
+        return _options.findInLabelMap(serie.metric) || defaultValue;
     }
-    return serie.metric.toString();
+    return defaultValue;
 }
 
 const selectBackGroundColor = (_options, serie, i) => {
+    const defaultValue = _options.backgroundColor[i % _options.backgroundColor.length];
     if (_options.findInBackgroundColorMap) {
-        return _options.findInBackgroundColorMap(serie.metric) || _options.backgroundColor[i % _options.backgroundColor.length];
+        return _options.findInBackgroundColorMap(serie.metric) || defaultValue;
     }
-    return _options.backgroundColor[i % _options.backgroundColor.length];
+    return defaultValue;
 }
 
 const selectBorderColor = (_options, serie, i) => {
+    const defaultValue = _options.borderColor[i % _options.borderColor.length];
     if (_options.findInBorderColorMap) {
-        return _options.findInBorderColorMap(serie.metric) || _options.borderColor[i % _options.borderColor.length];
+        return _options.findInBorderColorMap(serie.metric);
     }
-    return _options.borderColor[i % _options.borderColor.length];
+    return defaultValue;
 }
 
 export {
