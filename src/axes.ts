5340f02d73b495ee43b2c3849343edd69d181243
Michael Nutt <michael@nuttnet.net>
2023-03-22 15:18:54 -0400

    fix fillGaps to use x instead of t

--- src/axes.ts
+++ src/axes.ts
@@ -36,25 +36,25 @@ export function fillGaps(chart: Chart, start: Date, end: Date, step: number, opt
     chart.data.datasets.forEach((dataSet: ChartDataset, index: number) => {
         // detect missing data in response
         for (let i = dataSet.data.length - 2; i > 0; i--) {
-            if ((dataSet.data[i + 1]['t'] - dataSet.data[i]['t']) > (1100 * minStep)) {
-                for (let steps = (dataSet.data[i + 1]['t'] - dataSet.data[i]['t']) / (minStep * 1000); steps > 1; steps--) {
-                    const value = { t: new Date(dataSet.data[i + 1]['t'].getTime() - minStep * 1000), v: Number.NaN };
+            if ((dataSet.data[i + 1]['x'] - dataSet.data[i]['x']) > (1100 * minStep)) {
+                for (let steps = (dataSet.data[i + 1]['x'] - dataSet.data[i]['x']) / (minStep * 1000); steps > 1; steps--) {
+                    const value = { t: new Date(dataSet.data[i + 1]['x'].getTime() - minStep * 1000), v: Number.NaN };
                     (dataSet.data as any).splice(i + 1, 0, value);
                 }
             }
         }
 
         // at the start of time range
-        if (Math.abs(start.getTime() - dataSet.data[0]['t']) > (1100 * minStep)) {
-            for (let i = Math.abs(start.getTime() - dataSet.data[0]['t']) / (minStep * 1000); i > 1; i--) {
-                (chart.data.datasets[index].data as any).unshift({ t: new Date(dataSet.data[0]['t'].getTime() - minStep * 1000), v: Number.NaN });
+        if (Math.abs(start.getTime() - dataSet.data[0]['x']) > (1100 * minStep)) {
+            for (let i = Math.abs(start.getTime() - dataSet.data[0]['x']) / (minStep * 1000); i > 1; i--) {
+                (chart.data.datasets[index].data as any).unshift({ x: new Date(dataSet.data[0]['x'].getTime() - minStep * 1000), v: Number.NaN });
             }
         }
 
         // at the end of time range
-        if (Math.abs(end.getTime() - dataSet.data[dataSet.data.length - 1]['t']) > (1100 * minStep)) {
-            for (let i = Math.abs(end.getTime() - dataSet.data[dataSet.data.length - 1]['t']) / (minStep * 1000); i > 1; i--) {
-                (chart.data.datasets[index].data as any).push({ t: new Date(dataSet.data[chart.data.datasets[index].data.length - 1]['t'].getTime() + minStep * 1000), v: Number.NaN });
+        if (Math.abs(end.getTime() - dataSet.data[dataSet.data.length - 1]['x']) > (1100 * minStep)) {
+            for (let i = Math.abs(end.getTime() - dataSet.data[dataSet.data.length - 1]['x']) / (minStep * 1000); i > 1; i--) {
+                (chart.data.datasets[index].data as any).push({ x: new Date(dataSet.data[chart.data.datasets[index].data.length - 1]['x'].getTime() + minStep * 1000), v: Number.NaN });
             }
         }
     });
